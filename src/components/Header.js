0ccc0370f148a21f85b4e87f73b232d7b42031b7
Martin <escorpmartin97@gmail.com>
2022-03-05 16:05:59 -0300

    feat: passa informacao para o input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
+import Input from "./Input";
 import Link from "./Link";
 
 function Header() {
   return (
     <div>
+      <Link />
+
+      <Input
+        type="text"
+        name="filter"
+        pleaceholder="Qual o mimo de hoje para o seu anjinho?"
+        disabled={ false }
+      />
+
+      <Link />
       <Link />
     </div>
   );
