2a3ac7f6e55135309959a5ea522169f17433a123
Martin <escorpmartin97@gmail.com>
2022-03-06 00:36:04 -0300

    style: adiciona estilos na nav bar feat: adiciona links nos items de nevbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav >
-      <ul>
-        <li>PERFUME INFANTIL</li>
-        <li>SABONETES</li>
-        <li>PÓS-BANHO</li>
-        <li>ASSADURAS</li>
-        <li>HIDRATANTES</li>
-        <li>LENÇOS UMEDECIDOS</li>
+    <nav className="bg-secondary-color text-white">
+      <ul className="flex items-center h-12 justify-between p-6">
+        <li><a href="">PERFUME INFANTIL</a></li>
+        <li><a href="">SABONETES</a></li>
+        <li><a href="">PÓS-BANHO</a></li>
+        <li><a href="">ASSADURAS</a></li>
+        <li><a href="">HIDRATANTES</a></li>
+        <li><a href="">LENÇOS UMEDECIDOS</a></li>
       </ul>
     </nav>
   );
