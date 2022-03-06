35e2033460421bbe4f5c59888e57e1dd3f3e3d44
Martin <escorpmartin97@gmail.com>
2022-03-06 13:19:53 -0300

    fox: apaga componente que nao usarei feat: renderiza imagens na tela

--- src/components/CoverList.js
+++ src/components/CoverList.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import CoverCard from './CoverCard';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import PropTypes from 'prop-types';
 
 function CoverList({ covers }) {
-  console.log(covers)
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
-    <div>
+    <div className="">
+      <Slider {...settings}>
       {
         covers.map(({ id, name, img }) => {
-          return <CoverCard key={ id } src={ img } alt={ name } />
+          return <img className="" key={ id } src={ img } alt={ name } />
         })
       }
+      </Slider>
     </div>
   );
 }
