5a147108b7ea7c66834606fbfbc31aa2b09ecc71
Martin <escorpmartin97@gmail.com>
2022-03-06 00:15:53 -0300

    feat: cria componente navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>PERFUME INFANTIL</li>
+        <li>SABONETES</li>
+        <li>PÓS-BANHO</li>
+        <li>ASSADURAS</li>
+        <li>HIDRATANTES</li>
+        <li>LENÇOS UMEDECIDOS</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
