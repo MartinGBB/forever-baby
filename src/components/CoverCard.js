f25d2a628d23b11a108db34dbfb784297a3e78da
Martin <escorpmartin97@gmail.com>
2022-03-06 11:57:08 -0300

    feat: pega os dados do objeto no componente Coverkey

--- src/components/CoverCard.js
+++ src/components/CoverCard.js
@@ -4,6 +4,13 @@ import { covers } from '../data/covers';
 function CoverCard() {
   return (
     <div>
+      {
+        covers.map(({ id, img, name }) => {
+          <div key={ id }>
+            <img src={ img } alt={ name } />
+          </div>
+        })
+      }
     </div>
   );
 }
