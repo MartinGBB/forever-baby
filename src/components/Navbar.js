7ede518691714a9a5c0ac40369e326d28eabcc0b
Martin <escorpmartin97@gmail.com>
2022-03-06 01:46:37 -0300

    style: adiciona hover de sublinea

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ function Navbar() {
   return (
     <nav className="bg-secondary-color text-white">
       <ul className="desktop:flex desktop:justify-between p-6 phone:hidden phone:justify-start">
-        <li><a href="">PERFUME INFANTIL</a></li>
-        <li><a href="">SABONETES</a></li>
-        <li><a href="">PÓS-BANHO</a></li>
-        <li><a href="">ASSADURAS</a></li>
-        <li><a href="">HIDRATANTES</a></li>
-        <li><a href="">LENÇOS UMEDECIDOS</a></li>
+        <li className="hover:underline"><a href="">PERFUME INFANTIL</a></li>
+        <li className="hover:underline"><a href="">SABONETES</a></li>
+        <li className="hover:underline"><a href="">PÓS-BANHO</a></li>
+        <li className="hover:underline"><a href="">ASSADURAS</a></li>
+        <li className="hover:underline"><a href="">HIDRATANTES</a></li>
+        <li className="hover:underline"><a href="">LENÇOS UMEDECIDOS</a></li>
       </ul>
     </nav>
   );
