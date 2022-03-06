289465bb1435ea83a0cba4cddfbeb475533c9576
Martin <escorpmartin97@gmail.com>
2022-03-06 14:55:13 -0300

    style: estiliza com a cor da pagina

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function FavoritesCard(props) {
   const { image, title, price, classification } = props;
   return (
-    <div className="block p-5">
+    <div className="block p-5 bg-primary-color">
       <img className="" src={ image } alt={ title } />
       <h4 className="">{ title }</h4>
       <h5 className="">{ price }</h5>
