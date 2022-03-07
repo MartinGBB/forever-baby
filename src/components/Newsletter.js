a350fc068ca8df6f10abfe0d50488061a80ef59d
Martin <escorpmartin97@gmail.com>
2022-03-07 09:50:42 -0300

    feat: adiciona fincao que mostra para o cliente o que esta acontecendo no servidor e outra funcao que tira a mensagem depois de 3 segundos

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,29 +3,36 @@ import fetchApi from '../utils/fetch';
 
 function Newsletter() {
   const [email, setEmail] = useState("");
+  const [res, setRes] = useState("");
 
   const handleFetch = async () => {
   const endpoind = "https://foreverliss1.websiteseguro.com/teste-dev/newsletter.php"
   const method = "POST";
-  const response = fetchApi(endpoind, method, { email });
+  const request = await fetchApi(endpoind, method, { email });
+  const response = request.Mensagem ? setRes(request.Mensagem) : setRes(request.Erro);
   return response;
-  }
+};
+
+  const timeout = () => {
+    if (res !== "") return setRes("");
+  };
+  setTimeout(timeout, 3000);
 
   return (
   <div>
-      <input 
+      <input
         name="email"
         placeholder="Digite aqui o seu melhor e-mail"
         value={ email }
         onChange={ ({ target: { value } }) => setEmail(value) }
         />
-        {console.log(email)}
       <button
         type="button"
         onClick={ handleFetch }
       >
         CADASTRAR
       </button>
+      <p>{ res }</p>
     </div>
   );
 }
