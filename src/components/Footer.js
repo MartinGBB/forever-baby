4635621b49d8cf51a8aa9e452cc1343e27d9fc5f
Martin <escorpmartin97@gmail.com>
2022-03-07 19:23:35 -0300

    feat: adiciona e identa "direitos autoriais"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import paymentMethod from "../images/paymentMethod.png";
 
 function Footer({ contact }) {
   return (
+    <div>
     <div className="bg-[#F1F1F1]">
       <h1 className="p-8 text-quaternary-color">VISITE NOSSAS REDES SOCIAIS</h1>
       {
@@ -29,6 +30,17 @@ function Footer({ contact }) {
         <h1 className="text-quaternary-color">FORMAR DE PAGAMENTO</h1>
         <img className="m-auto pb-2" src={ paymentMethod } alt="Forma de pagamento"/>
       </div>
+    </div>
+          <div className="bg-white w-auto m-2 pl-36 pr-36 mb-8">
+          <span >
+            <p>2022 www.foreverliss.com.br</p>
+            <p>Todos os direitos reservados.</p>
+            <p>
+              Proibida reprodução total ou parcial. IMPORTANTE! Os preços e estoque sujeitos a alterações e podem variar sem aviso prévio. Em caso de divergência no valor do produto, irá prevalecer sempre o valor do carrinho de compras.
+              FOREVER COMPANY COSMETICOS LTDA / www.foreverliss.com.br / Rua José Bonifácio, N° 690 Lençóis Paulista - SP Cep: 18683-420 | CNPJ: 08.958.817/0001-89 | Inscrição Estadual: 416.118.979.112
+            </p>
+          </span>
+        </div>
     </div>
   );
 }
