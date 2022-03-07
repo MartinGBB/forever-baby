139668e505e3fdb0f83f4c71669442d2559b2ebf
Martin <escorpmartin97@gmail.com>
2022-03-07 14:16:48 -0300

    feat: cria objeto

---
+++ src/data/contact.js
@@ -0,0 +1,29 @@
+import instagram from '../images/instagram.png';
+import facebook from '../images/facebook.png';
+import youtube from '../images/youtube.png';
+import tiktok from '../images/tiktok.png';
+
+const contact = [
+  {
+    id: 1,
+    name: "instagram",
+    image: instagram,
+  },
+  {
+    id: 2,
+    name: "facebook",
+    image: facebook,
+  },
+  {
+    id: 3,
+    name: "youtube",
+    image: youtube,
+  },
+  {
+    id: 4,
+    name: "tiktok",
+    image: tiktok,
+  }
+];
+
+export default contact;
