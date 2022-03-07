c91eb995359519f522a9ecbec8dfd72d8e9d4c16
Martin <escorpmartin97@gmail.com>
2022-03-07 14:26:49 -0300

    feat: adiciona lik de redes sociales

--- src/data/contact.js
+++ src/data/contact.js
@@ -8,21 +8,25 @@ const contact = [
     id: 1,
     name: "instagram",
     image: instagram,
+    link: "https://www.instagram.com/foreverliss/"
   },
   {
     id: 2,
     name: "facebook",
     image: facebook,
+    link: "https://www.facebook.com/foreverliss"
   },
   {
     id: 3,
     name: "youtube",
     image: youtube,
+    link: "https://www.youtube.com/c/ForeverLissOficial"
   },
   {
     id: 4,
     name: "tiktok",
     image: tiktok,
+    link: "https://www.tiktok.com/@foreverlissprofessional"
   }
 ];
 
