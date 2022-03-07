143ab7a3ab702dff6f16b08123a504678a78dbae
Martin <escorpmartin97@gmail.com>
2022-03-07 01:29:25 -0300

    feat: implementa setState paa pegar email

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import fetchApi from '../utils/fetch';
 
 function Newsletter() {
+  const [email, setEmail] = useState("");
+
   const handleFetch = async (data) => {
   const endpoind = "https://foreverliss1.websiteseguro.com/teste-dev/newsletter.php"
   const method = "POST";
@@ -10,7 +12,12 @@ function Newsletter() {
 
   return (
     <div>
-
+      <input 
+        type="email"
+        name="email"
+        placeholder="Digite aqui o seu melhor e-mail"
+        onChange={ ({ target: value }) => setEmail(value) }
+      />
     </div>
   );
 }
