35f8efd2cd8a60411056b1dae27c288a63662a39
Martin <escorpmartin97@gmail.com>
2022-03-06 21:20:21 -0300

    feat: adiciona imagenes e renderiza elas

--- src/data/favoritesData.js
+++ src/data/favoritesData.js
@@ -1,5 +1,10 @@
-import bloks from "../images/bloks.jpg";
 import starts from "../images/starts.png";
+import bloks from "../images/bloks.jpg";
+import shampoo from "../images/shampoo.jpg";
+import urso from "../images/urso.jpg";
+import dooks from "../images/dooks.jpg";
+import cars from "../images/cars.jpg";
+import clothing from "../images/clothing.jpg";
 
 const favorites = [
   {
@@ -11,39 +16,44 @@ const favorites = [
   },
   {
     id: 2,
-    image: "",
-    title: "Product 2",
+    image: shampoo,
+    title: "Champú",
     price: "R$ 0.00",
     classification: starts
   },
   {
     id: 3,
-    image: "",
-    title: "",
+    image: urso,
+    title: "Urso",
     price: "R$ 0.00",
     classification: starts
   },
   {
     id: 4,
-    image: "",
-    title: "",
+    image: dooks,
+    title: "Patos",
     price: "R$ 0.00",
     classification: starts
   },
   {
     id: 5,
-    image: "",
-    title: "",
+    image: cars,
+    title: "cars",
     price: "R$ 0.00",
     classification: starts
   },
   {
     id: 6,
-    image: "",
-    title: "",
+    image: clothing,
+    title: "Roupinha",
     price: "R$ 0.00",
     classification: starts
   },
 ]
 
 export default favorites;
+
+// referens: 
+// https://www.google.com/search?q=roupa+bebes+images&tbm=isch&ved=2ahUKEwjWn6Lq1rL2AhX9SbgEHfSGD_cQ2-cCegQIABAA&oq=roupa+bebes+images&gs_lcp=CgNpbWcQAzoHCCMQ7wMQJ1CYAliyDmChD2gAcAB4AIABigGIAc4GkgEDMC43mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=AkklYtaWF_2T4dUP9I2-uA8&bih=602&biw=1360#imgrc=ZoJdW6PYING50M
+// https://es.dhgate.com/product/30ml-empty-container-bottle-dispenser-shampoo/539008659.html
+// https://www.sitedebelezaemoda.com.br/
