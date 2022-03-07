eb166c2f4550c7a403960258c193a85ce962edf0
Martin <escorpmartin97@gmail.com>
2022-03-07 20:07:37 -0300

    feat: notificacao que o item se adicionou ao carrinho

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
-import Button from './Button';
+// import Button from './Button';
 
 function FavoritesCard(props) {
+  const [buy, setBuy] = useState("");
+  const handdleBuys = () => {
+    setBuy("Seu produto foi adicionado ao carrinho");
+  };
+
   const { image, title, price, classification } = props;
   return (
     <div className="block justify-center w-72 bg-white m-2 h-96 border-b-4 border-b-secondary-color">
+      <p className="bg-[#ff8800bb] text-white m-1 absolute rounded-lg">{ buy }</p>
       <a href="/"><img className="w-72 h-15" src={ image } alt={ title } /></a>
       <a href="/"><h4 className="p-1 mt-2 text-quaternary-color">{ title }</h4></a>
       <h5 className="mb-3 text-tertiary-color">{ price }</h5>
       <img className="justify-center flex w-28 m-auto" src={ classification } alt="clasification" />
-      <Button
+      <button
+        type="button"
+        className="w-56 h-9 bg-[#FF8A00] text-white rounded-md mt-2"
+        onClick={ handdleBuys }
+      >
+        Adicionar
+      </button>
+      {/* <Button
         type="button"
         disabled={ false }
         style="w-56 h-9 bg-[#FF8A00] text-white rounded-md mt-2"
         name="Adicionar"
-      />
+      /> */}
     </div>
   );
 }
