9dbb5d251026cc69d4f4ef6808f2fe60a46c3a2d
Martin <escorpmartin97@gmail.com>
2022-03-07 21:37:46 -0300

    feat: valida se o campo de busca tem menos de 5 caracteres e se esta vazio

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,13 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 function Input(props) {
-  const { type, name, pleaceholder, disabled, style } = props;
+  const { type, name, pleaceholder, style } = props;
   return (
     <input
       type={ type }
       name={ name }
       placeholder={ pleaceholder }
-      disabled={ disabled }
       className={ style }
     />
   );
@@ -19,7 +18,6 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   pleaceholder: PropTypes.string.isRequired,
   style: PropTypes.string.isRequired,
-  disabled: PropTypes.bool.isRequired,
 }
 
 export default Input;
